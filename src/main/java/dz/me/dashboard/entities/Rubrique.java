3d49439656171cb55c47e46bd83e4c8e4a59861f
Dr. Latif <esselami.abdellatif@hotmail.fr>
2022-12-23 00:44:44 +0100

    added rubrique values for rubrique

--- src/main/java/dz/me/dashboard/entities/Rubrique.java
+++ src/main/java/dz/me/dashboard/entities/Rubrique.java
@@ -1,13 +1,17 @@
 package dz.me.dashboard.entities;
 
+import java.util.Set;
 import java.util.UUID;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
@@ -42,4 +46,6 @@ public class Rubrique {
     @JoinColumn(name = "id_service")
     private Service service;
 
+    @OneToMany(mappedBy = "rubrique", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<RubriqueValue> rubriqueValues;
 }
